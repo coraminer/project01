866bfcd27316083d184b8af606a9813a5c03fa9c
cora <1574213651@qq.com>
2019-01-22 12:29:41 +0800

    Initial commit

---
+++ src/com/gerat/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.gerat.hello;
+
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println();
+        System.out.println("hello world");
+
+    }
+
+
+}
